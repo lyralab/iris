8dfbec95874a5396244baee812f257d04331f108
borhanisaleh6 <borhanisaleh6@gmail.com>
2025-08-28 14:38:28 +0330

    feat: add send notif to groups and save state in db

--- migrations/000001_create_alerts_table.up.sql
+++ migrations/000001_create_alerts_table.up.sql
@@ -5,5 +5,13 @@ CREATE TABLE IF NOT EXISTS alerts (
     description text,
     starts_at timestamp,
     ends_at timestamp,
-    status VARCHAR(10)
+    status VARCHAR(10),
+    method VARCHAR(10),
+    receptor VARCHAR(100),
+    send_notif BOOLEAN DEFAULT FALSE,
+    silenced INT DEFAULT 0
 );
+
+CREATE INDEX IF NOT EXISTS idx_alerts_send_notif_false
+    ON alerts (id)
+    WHERE send_notif = false;
